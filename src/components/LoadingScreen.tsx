56d61b26ba9ef3f4028900e80b90bbce24e101e3
Vincent Abesamis <vincentabesamis03@gmail.com>
2021-06-02 08:15:29 +0800

    Style loading screen

--- src/components/LoadingScreen.tsx
+++ src/components/LoadingScreen.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react'
 
 const LoadingScreen = () => (
-  <div className="bg-gray-200 min-h-screen w-full flex justify-center items-center">
+  <div className="bg-gray-200 min-h-screen w-full flex justify-center items-center text-xl tracking-wider font-medium text-gray-700">
     Loading...
   </div>
 )
